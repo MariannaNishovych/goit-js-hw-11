c73f26fb5132958bcc13c26c3c3464a06002afb8
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-09-23 12:22:37 +0300

    add style

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,10 @@
 
 const API_KEY = '46087000-8dffe905d5bdd270a8d626623';
-const BASE_URL = `https://${API_KEY}.pixabay.com/api/`;
+const BASE_URL = 'https://pixabay.com/api/';
 
 export function searchElement(element) {
 
-    const url = `${BASE_URL}?q=${element}&image_type=photo&orientation=horizontal&safesearch=true`
+    const url = `${BASE_URL}?key=${API_KEY}&q=${element}&image_type=photo&orientation=horizontal&safesearch=true`
 
     return fetch(url)
         .then(response => {
--- src/main.js
+++ src/main.js
@@ -1,25 +1,65 @@
-'use strict'
+'use strict'  
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';  
+import 'izitoast/dist/css/iziToast.min.css';  
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';  
+import 'simplelightbox/dist/simple-lightbox.min.css';  
 
-import { searchElement } from './js/pixabay-api';
-import { createMarkup } from './js/render-function';
+import { searchElement } from './js/pixabay-api';  
+import { createMarkup } from './js/render-function';  
 
-const refs = {
-    gallaryEl: document.querySelector('.gallery'),
-    formEl: document.querySelector('form'),
-    loaderEl: document.querySelector('.loading'),
-}
+const galleryEl = document.querySelector('.gallery');  
+const form = document.querySelector('form');  
+const loader = document.querySelector('.loading');  
 
-let galleryBox = new SimpleLightbox('.galleryEl a', {
-                    caption: true,
-                    captionDelay: 250,
-                    captionsData: 'alt',
-});
+let galleryBox = new SimpleLightbox('.gallery a', {  
+    caption: true,  
+    captionDelay: 250,  
+    captionsData: 'alt',  
+});  
+
+function findElement() {  
+    form.addEventListener('submit', event => {  
+        event.preventDefault();  
+        
+        galleryEl.innerHTML = '';  
+
+        const inputElement = event.target.elements.search.value.toLowerCase().trim();  
+
+        if (!inputElement) {  
+            iziToast.error({ message: 'Please enter a search word.' });  
+            return;  
+        }  
+
+        
+        loader.style.display = 'block';  
+
+        
+        searchElement(inputElement)  
+            .then(element => {  
+                
+                if (element.hits.length === 0) {  
+                    iziToast.info({  
+                        message:  
+                        'Sorry, there are no images matching your search query. Please try again!',  
+                    });  
+                } else {  
+                    createMarkup(element.hits, galleryEl);  
+                    galleryBox.refresh();  
+                }  
+            })  
+            .catch(error => {  
+                iziToast.error({ message: error.message });  
+            })  
+            .finally(() => {  
+                loader.style.display = 'none';  
+                form.reset();  
+            });  
+    });  
+}  
+
+findElement();  
                     
 
 
